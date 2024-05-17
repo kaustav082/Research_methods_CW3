51eb44e0913f2a20061ca68dbba464c096f84509
kaustav082 <134273871+kaustav082@users.noreply.github.com>
2024-05-17 14:02:38 +0100

    Hi

---
+++ data/generated/bib.js
@@ -0,0 +1,192 @@
+﻿define({ entries : {
+    "Machine_learning_1": {
+        "author": "Wanjun Xia, Yanping Jiang, Xiaohong Chen, and Rui Zhao",
+        "doi": "10.1177/0734242X211033716",
+        "journal": "ISWA Waste Management & Research: The Journal for a Sustainable Circular Economy",
+        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
+        "publisher": "International Solid Waste Association",
+        "title": "Application of machine learning algorithms in municipal solid waste management: A mini review",
+        "type": "article",
+        "url": "https://journals.sagepub.com/doi/10.1177/0734242X211033716",
+        "year": "2021"
+    },
+    "Machine_learning_10": {
+        "author": "Kenneth Li-Minn Ang, Jasmine Kah Phooi Seng, Ericmoore Ngharamike, Gerald K. Ijemaru",
+        "doi": "10.3390/ijgi11020085",
+        "journal": "ISPRS International Journal of Geo-Information",
+        "keywords": "geo-information, transportation, smart cities, machine learning, data analytics, big data, deep learning, artificial intelligence (AI)",
+        "publisher": "mdpi",
+        "title": "Emerging Technologies for Smart Cities Transportation:Geo-Information, Data Analytics and Machine Learning Approaches",
+        "type": "article",
+        "url": "https://www.mdpi.com/2220-9964/11/2/85",
+        "year": "2022"
+    },
+    "Machine_learning_11": {
+        "author": "de la Torre, R.; Corlu, C.G.; Faulin, J.; Onggo, B.S.; Juan, A.A",
+        "doi": "10.3390/su13031551",
+        "journal": "sustainability",
+        "publisher": "mdpi",
+        "title": "Simulation, Optimization, and Machine Learning in Sustainable Transportation Systems: Models and Applications",
+        "type": "article",
+        "url": "https://www.mdpi.com/2071-1050/13/3/1551",
+        "year": "2020"
+    },
+    "Machine_learning_12": {
+        "author": "Zantalis F, Koulouras G, Karabetsos S, Kandris D",
+        "doi": "10.3390/fi11040094",
+        "journal": "Future Internet",
+        "publisher": "mdpi",
+        "title": "A Review of Machine Learning and IoT in Smart Transportation",
+        "type": "article",
+        "url": "https://www.mdpi.com/1999-5903/11/4/94",
+        "year": "2019"
+    },
+    "Machine_learning_13": {
+        "author": "Lansky J, Rahmani AM, Hosseinzadeh M",
+        "doi": "10.3390/math10244673",
+        "journal": "Mathematics",
+        "publisher": "mdpi",
+        "title": "Reinforcement Learning-Based Routing Protocols in Vehicular Ad Hoc Networks for Intelligent Transport System (ITS): A Survey",
+        "type": "article",
+        "url": "https://www.mdpi.com/2227-7390/10/24/4673",
+        "year": "2022"
+    },
+    "Machine_learning_14": {
+        "author": "Pamu\u0142a, T.; Pamu\u0142a, W",
+        "doi": "10.3390/en13092340",
+        "journal": "Energies",
+        "publisher": "mdpi",
+        "title": "Estimation of the Energy Consumption of Battery Electric Buses for Public Transport Networks Using Real-World Data and Deep Learning",
+        "type": "article",
+        "url": "https://www.mdpi.com/1996-1073/13/9/2340",
+        "year": "2020"
+    },
+    "Machine_learning_15": {
+        "author": "Adi TN, Bae H, Iskandar YA",
+        "doi": "10.3390/pr9101728",
+        "journal": "Processes",
+        "publisher": "mdpi",
+        "title": "Interterminal Truck Routing Optimization Using Cooperative Multiagent Deep Reinforcement Learning",
+        "type": "article",
+        "url": "https://www.mdpi.com/2227-9717/9/10/1728",
+        "year": "2021"
+    },
+    "Machine_learning_16": {
+        "author": "Laynes-Fiascunari V, Gutierrez-Franco E, Rabelo L, Sarmiento AT, Lee G",
+        "doi": "10.3390/app13105888",
+        "journal": "Applied Sciences",
+        "publisher": "mdpi",
+        "title": " A Framework for Urban Last-Mile Delivery Traffic Forecasting: An In-Depth Review of Social Media Analytics and Deep Learning Techniques",
+        "type": "article",
+        "url": "https://www.mdpi.com/2076-3417/13/10/5888",
+        "year": "2023"
+    },
+    "Machine_learning_17": {
+        "author": "Schroeder M, Lodemann S",
+        "doi": "10.3390/logistics5030062",
+        "journal": "Logistics",
+        "publisher": "mdpi",
+        "title": " A Systematic Investigation of the Integration of Machine Learning into Supply Chain Risk Management",
+        "type": "article",
+        "url": "https://www.mdpi.com/2305-6290/5/3/62",
+        "year": "2023"
+    },
+    "Machine_learning_18": {
+        "author": "M. R. Shoaib, H. M. Emara, J. Zhao",
+        "doi": "10.1109/ICCA59364.2023.10401518",
+        "journal": "ICCA",
+        "publisher": "IEEE",
+        "title": " A Survey on the Applications of Frontier AI, Foundation Models, and Large Language Models to Intelligent Transportation Systems",
+        "type": "article",
+        "url": "https://ieeexplore.ieee.org/abstract/document/10401518",
+        "year": "2023"
+    },
+    "Machine_learning_2": {
+        "author": "Manuel Woschank, Erwin Rauch, Helmut Zsifkovits",
+        "doi": "10.3390/su12093760",
+        "journal": "sustainability",
+        "keywords": "industry 4.0, artificial intelligence, machine learning, deep learning, smart logistics,logistics 4.0",
+        "publisher": "mdpi",
+        "title": "A Review of Further Directions for Artificial Intelligence, Machine Learning, and Deep Learning in Smart Logistics",
+        "type": "article",
+        "url": "https://www.mdpi.com/2071-1050/12/9/3760",
+        "year": "2020"
+    },
+    "Machine_learning_3": {
+        "author": "Canan G. Corlu, Rocio de la Torre, Adrian Serrano-Hernandez, Angel A. Juan ,Javier Faulin",
+        "doi": "10.3390/en13051115",
+        "journal": "energies",
+        "keywords": "transportation systems, optimization problems, efficient energy consumption, intelligent algorithms, sustainability",
+        "publisher": "mdpi",
+        "title": "Optimizing Energy Consumption in Transportation: Literature Review, Insights, and Research Opportunities",
+        "type": "article",
+        "url": "https://www.mdpi.com/1996-1073/13/5/1115",
+        "year": "2020"
+    },
+    "Machine_learning_4": {
+        "author": "\u00c5. Jevinger,C. Zhao,J. A. Persson ,P. Davidsson",
+        "doi": "10.1007/s12469-023-00334-7",
+        "journal": "public transport",
+        "keywords": "Artifcial intelligence ,Machine learning ,Public transit ,Mass transit ,Public transport ,Literature review",
+        "publisher": "springerlink",
+        "title": "Artificial intelligence for improving public transport: a mapping study",
+        "type": "article",
+        "url": "https://link.springer.com/article/10.1007/s12469-023-00334-7",
+        "year": "2023"
+    },
+    "Machine_learning_5": {
+        "author": "SUROJIT GUPTA ,LAN LI",
+        "doi": "10.1007/s11837-021-05079-x",
+        "journal": "JOM",
+        "keywords": "Artifcial intelligence ,Machine learning ,Public transit ,Mass transit ,Public transport ,Literature review",
+        "publisher": "The Minerals, Metals & Materials Society",
+        "title": "The Potential of Machine Learning for Enhancing CO2 Sequestration, Storage, Transportation, and Utilization-based Processes: A Brief Perspective",
+        "type": "article",
+        "url": "https://link.springer.com/article/10.1007/s11837-021-05079-x",
+        "year": "2022"
+    },
+    "Machine_learning_6": {
+        "author": "Nadia Giuffrida,Jenny Fajardo-Calderin,Antonio D. Masegosa,Frank Werner,Margarete Steudter,Francesco Pilla",
+        "doi": "10.3390/su14095329",
+        "journal": "sustainability",
+        "keywords": "industry 4.0, artificial intelligence, machine learning, deep learning, smart logistics",
+        "publisher": "mdpi",
+        "title": "Optimization and Machine Learning Applied to Last-Mile Logistics: A Review",
+        "type": "article",
+        "url": "https://www.mdpi.com/2071-1050/14/9/5329",
+        "year": "2022"
+    },
+    "Machine_learning_7": {
+        "author": "Kalliopi Tsolaki, Thanasis Vafeiadis, Alexandros Nizamis, Dimosthenis Ioannidis, Dimitrios Tzovaras",
+        "doi": "10.1016/j.icte.2022.02.001",
+        "journal": "ICT Express",
+        "keywords": "industry 4.0, artificial intelligence, machine learning, deep learning, smart logistics",
+        "publisher": "sciencedirect",
+        "title": "Utilizing machine learning on freight transportation and logistics applications: A review",
+        "type": "article",
+        "url": "https://www.sciencedirect.com/science/article/pii/S2405959522000200?via%3Dihub",
+        "year": "2023"
+    },
+    "Machine_learning_8": {
+        "author": "Yue Hou , Qiuhan Li, Chen Zhang, Guoyang Lu, Zhoujing Ye, Yihan Chen, Linbing Wang, Dandan Cao",
+        "doi": "10.1016/j.eng.2020.07.030",
+        "journal": "Engineering",
+        "keywords": "Pavement monitoring and analysis,The state-of-the-art review,Intrusive sensing,Image processing techniques,Machine learning methods",
+        "publisher": "Chinese Academy of Engineering",
+        "title": "The State-of-the-Art Review on Applications of Intrusive Sensing, Image Processing Techniques, and Machine Learning Methods in Pavement Monitoring and Analysis",
+        "type": "article",
+        "url": "https://www.sciencedirect.com/science/article/pii/S2095809920303799?via%3Dihub",
+        "year": "2021"
+    },
+    "Machine_learning_9": {
+        "author": "Rafidah Md Noor, Nadia Bella Gustiani Rasyidi, Tarak Nandy,Raenu Kolandaisamy",
+        "doi": "10.3390/su13010225",
+        "journal": "sustainability",
+        "keywords": "time prediction; machine learning; ANN; SVM; shuttle bus; route optimization",
+        "publisher": "mdpi",
+        "title": "Campus Shuttle Bus Route Optimization Using Machine Learning Predictive Analysis: A Case Study",
+        "type": "article",
+        "url": "https://www.mdpi.com/2071-1050/14/9/5329",
+        "year": "2020"
+    }
+}});
